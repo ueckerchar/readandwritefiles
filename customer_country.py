f017949354b4b6aaf65f1fa142e0b1a4b70043fe
Charlie <Charles_Uecker1@baylor.edu>
2022-09-06 13:01:55 -0500

    Customer Country Changes

--- customer_country.py
+++ customer_country.py
@@ -4,18 +4,23 @@ def main():
     import csv
 
     infile = open('customers.csv','r')
+    outfile = open('customer_country.csv','w')
 
     csvfile = csv.reader(infile, delimiter=',')
 
     next(csvfile)
 
+    outfile.write("Full Name, Country\n")
+
     for record in csvfile:
-        print(record[1],'',record[2],',',record[4])
+        full_name = record[1] + " " + record[2]
+        country = record[4]
 
-    outfile = open('customer_country.csv','w')
+        outfile.write(full_name + ',' + country + '\n')
+        
 
+    outfile.close()
 
-    outfile.write(csvfile)
 
 
 
